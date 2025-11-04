19237a6f21c59c5c3cf5800e41d66b8612f40e5c
LAPTOP-E769C15R\AnnaK <anna.kovalchuk.23@yandex.ru>
2025-11-04 22:47:45 +0300

    iter8 добавление сжатия

--- internal/handler/middleware.go
+++ internal/handler/middleware.go
@@ -1,33 +1,52 @@
 package handler
 
 import (
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/sirupsen/logrus"
 )
 
-type responseLogger struct {
+type ResponseCompressLogger struct {
 	http.ResponseWriter
+	header http.Header
 	status int
 	size   int
+	buf    *bytes.Buffer
 }
 
-func (l *responseLogger) WriteHeader(code int) {
+func (l *ResponseCompressLogger) WriteHeader(code int) {
 	if l.status != 0 {
-		return // Уже установлен — ничего не делаем
+		return
 	}
 	l.status = code
 	l.ResponseWriter.WriteHeader(code)
 }
 
-func (l *responseLogger) Write(b []byte) (int, error) {
+func (l *ResponseCompressLogger) Header() http.Header {
+	return l.header
+}
+
+func (l *ResponseCompressLogger) Write(b []byte) (int, error) {
 	if l.status == 0 {
 		l.status = http.StatusOK
 	}
-	size, err := l.ResponseWriter.Write(b)
+	size, err := l.buf.Write(b)
 	l.size += size
 	return size, err
+}
+
+func NewResponseCompressLogger(w http.ResponseWriter) *ResponseCompressLogger {
+	return &ResponseCompressLogger{
+		header: make(http.Header),
+		status: http.StatusOK,
+		buf:    new(bytes.Buffer),
+	}
 }
 
 func (s *Server) LoggingMiddleware(next http.Handler) http.HandlerFunc {
@@ -36,16 +55,92 @@ func (s *Server) LoggingMiddleware(next http.Handler) http.HandlerFunc {
 		uri := r.URL.RequestURI()
 		method := r.Method
 
-		logger := &responseLogger{ResponseWriter: w}
+		newRequest, err := s.decompressRequest(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
 
-		next.ServeHTTP(logger, r)
+		writer := NewResponseCompressLogger(w)
+
+		next.ServeHTTP(writer, newRequest)
+
+		acceptEncoding := r.Header.Get("Accept-Encoding")
+		contentType := writer.header.Get("Content-Type")
+		var needCompress bool
+		if strings.Contains(acceptEncoding, "gzip") &&
+			(contentType == "application/json" || contentType == "text/html") {
+			needCompress = true
+		}
+
+		for k, vv := range writer.header {
+			for _, v := range vv {
+				w.Header().Add(k, v)
+			}
+		}
+		var body []byte
+		if needCompress {
+			w.Header().Set("Content-Encoding", "gzip")
+			var err error
+			body, err = s.compessResponse(writer)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+		} else {
+			body = writer.buf.Bytes()
+		}
+
+		w.WriteHeader(writer.status)
+
+		if _, err := w.Write(body); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
 
 		logrus.WithFields(logrus.Fields{
 			"method":      method,
 			"uri":         uri,
-			"status":      logger.status,
+			"status":      writer.status,
 			"duration":    time.Since(start).String(),
-			"content_len": logger.size,
+			"content_len": writer.size,
 		}).Info("Запрос выполнен")
 	})
+}
+
+func (s *Server) decompressRequest(r *http.Request) (*http.Request, error) {
+	if r.Header.Get("Content-Encoding") == "gzip" &&
+		(r.Header.Get("Content-Type") == "application/json" || r.Header.Get("Content-Type") == "text/html") {
+		gz, err := gzip.NewReader(r.Body)
+		if err != nil {
+			return nil, fmt.Errorf("%s", "Ошибка декодирования gzip")
+		}
+		defer gz.Close()
+
+		decompressedBody, err := io.ReadAll(gz)
+		if err != nil {
+			return nil, fmt.Errorf("%s", "Ошибка чтения разжатого тела")
+		}
+		newReq := r.Clone(r.Context())
+		newReq.Body = io.NopCloser(bytes.NewBuffer(decompressedBody))
+		newReq.ContentLength = int64(len(decompressedBody))
+		newReq.Header.Del("Content-Encoding")
+		return newReq, nil
+	} else {
+		return r, nil
+	}
+}
+
+func (s *Server) compessResponse(resp *ResponseCompressLogger) ([]byte, error) {
+	var gzipBuf bytes.Buffer
+	gz := gzip.NewWriter(&gzipBuf)
+	_, err := gz.Write(resp.buf.Bytes())
+	if err != nil {
+		return nil, err
+	}
+	err = gz.Close()
+	if err != nil {
+		return nil, err
+	}
+	return gzipBuf.Bytes(), nil
 }
